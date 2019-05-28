ae6da555f8d49c1679d844c9384df8515b07f022
Daryl <daryl@builtbyelement.io>
2019-05-28 15:20:41 +0100

    Added new type of form to validate, but to also just pass data back through onSubmit. Then react like a normal form if onsubmit doesnt exist.

---
+++ src/core/reactform/Form.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import { serialize } from '../../utils/tools.js';
+import ValidationGroup from '../validation_group/ValidationGroup';
+
+/**
+ * Set up type declarations
+ */
+
+export interface FormProps extends React.FormHTMLAttributes<HTMLFormElement> {
+    children?: React.ReactNode;
+
+    onSubmit: (formData: any) => void;
+}
+
+class Form extends React.Component<FormProps, {}> {
+    constructor(props) {
+        super(props);
+    }
+
+    private validationGroupRef: React.RefObject<ValidationGroup> = React.createRef();
+
+    /**
+     * onSubmitCallback - called after form is submitted.
+     */
+    private handle_submit = (e): void => {
+        if (this.props.onSubmit) {
+            e.preventDefault();
+
+            const isValid = this.validationGroupRef.current.validateAllInputs();
+
+            if (isValid) {
+                this.props.onSubmit(serialize(e.currentTarget) as object);
+            }
+        }
+    };
+
+    /**
+     * render method, primary template method..
+     */
+    public render(): JSX.Element {
+        const { children } = this.props;
+
+        return (
+            <form {...this.props} onSubmit={this.handle_submit}>
+                <ValidationGroup ref={this.validationGroupRef}>{children}</ValidationGroup>
+            </form>
+        );
+    }
+}
+
+export default Form;
